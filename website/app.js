80dffb27bbfaacc8c0464eccfd255dfa87d94733
Youssef Girgeis <youssefgirgeis@gmail.com>
2021-10-20 21:06:29 -0400

    create async function to update the UI

--- website/app.js
+++ website/app.js
@@ -2,7 +2,9 @@
 const url = "http://api.openweathermap.org/data/2.5/weather?zip=&&appid=";
 const API_KEY = "ff9ed2b3aa83f2ca46f9febbe9781413";
 const generateButton = document.getElementById("generate");
-
+const temperatureHTML = document.getElementById("temp");
+const dateHTML = document.getElementById("date");
+const contentHTML = document.getElementById("content");
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth() + "." + d.getDate() + "." + d.getFullYear();
@@ -16,7 +18,7 @@ generateButton.addEventListener("click", function () {
       temperature: temperature,
       date: newDate,
       feelings: feelings,
-    });
+    }).then((data) => updateUI(data));
   });
 });
 
@@ -43,9 +45,14 @@ const postData = async function (url = "", data = {}) {
 
   try {
     const newData = await response.json();
-    console.log(newData);
     return newData;
   } catch (err) {
     console.log(err);
   }
+};
+
+const updateUI = async function (data) {
+  temperatureHTML.innerHTML = data.temperature;
+  dateHTML.innerHTML = data.date;
+  contentHTML.innerHTML = data.userResponse;
 };
